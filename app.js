f0143a0549369a4476370600bb4077a78caa7d1a
eshita95 <eshita.islam95@gmail.com>
2022-02-16 15:34:06 +0600

    total calculated price added

--- app.js
+++ app.js
@@ -13,8 +13,29 @@ function updateProductNumber(product, price, isIncreasing){
     const productPrice = document.getElementById(product + '-price');
     productPrice.innerText = productNumber * price;
 
+    // calculate total
+    calculateTotal();
+
 }
 
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt( productInput.value);
+    return productNumber;
+}
+
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const taxTotal = subTotal / 10;
+    const totalPrice = subTotal + taxTotal;
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-total').innerText = taxTotal;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
+/* phone price update */
 document.getElementById('phone-plus').addEventListener('click', function(){
     updateProductNumber('phone', 1219 , true);
 })
@@ -23,6 +44,7 @@ document.getElementById('phone-minus').addEventListener('click', function(){
     updateProductNumber('phone', 1219, false);
 })
 
+/* case price update */
 document.getElementById('case-plus').addEventListener('click', function(){
     updateProductNumber('case', 59 , true);
 })
